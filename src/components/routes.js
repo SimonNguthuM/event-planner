d78abf7897af2425a6c45e2a6c6c577b2bf61482
joel_patrick <jayjoelpn@gmail.com>
2024-08-13 20:29:16 +0300

    Initial commit

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,15 +1,24 @@
 import Home from "../pages/Home";
 import RSVPForm from "../pages/RSVPForm"
+import EventDetails from "../pages/EventDetails";
+
 
 const routes = [
     {
       path: "/",
       element: <Home />,
     },
+
     {
       path: "/events/:id/rsvp",
       element: <RSVPForm />,
+    },
+
+    {
+      path: "/events/:id",  
+      element: <EventDetails />,
     }
+
   ];
   
   export default routes;
